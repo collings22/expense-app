69127a1260042856afeae31736d35004322876e4
Daniel Collings <collings8915@gmail.com>
2020-07-05 21:07:05 +0100

    Change layout

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ import { camera, trash, close } from 'ionicons/icons';
 import {
   IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
   IonFab, IonFabButton, IonIcon, IonGrid, IonRow,
-  IonCol, IonImg, IonInput, IonLabel, IonItem
+  IonCol, IonImg, IonInput, IonLabel, IonItem, IonButton, IonText, IonCard, IonCardContent
 } from '@ionic/react';
 
 const Home: React.FC = () => {
@@ -22,34 +22,49 @@ const Home: React.FC = () => {
           <IonTitle>Expense App</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-        <IonGrid>
-          <IonRow>
-            {photos.map((photo, index) => (
-              <IonCol size="6" key={index}>
-                <IonImg src={photo.base64 ?? photo.webviewPath} />
+      <IonCard>
+        <IonCardContent>
+          <IonText>Enter expenses into the input fields and use the update button to log how much is claimable against that receipt.</IonText>
+          <IonTitle>
+            Expense Total: £{photos.reduce(
+            (accumulator, currentValue) => parseFloat(accumulator.toString()) + parseFloat(currentValue.expense)
+            , 0
+          )}
+          </IonTitle>
+        </IonCardContent>
+      </IonCard>
+
+
+      <IonGrid>
+        <IonRow>
+
+          {photos.map((photo, index) => (
+            <IonCard>
+              <IonCardContent>
+                <IonCol size="6" key={index}>
+                  <IonImg src={photo.base64 ?? photo.webviewPath} />
                   <IonLabel>Timestamp: {photo.timestamp}</IonLabel>
 
+
                   <IonItem>
-                    <IonLabel>Enter expense: £</IonLabel>
-                    <IonInput type="number" step="0.01" min="0" placeholder={photo.expense} onPointerLeave={e => updateChange(e.target)}></IonInput>
-                    <a type="btn" onClick={e => updateExpense(photo.filepath, expense)}>Update</a>
-                  </IonItem> 
-                         
-              </IonCol>
-            ))}
-          </IonRow>
-        </IonGrid>
-      </IonContent>
+                    £<IonInput type="number" step="0.01" min="0" placeholder={photo.expense} onPointerLeave={e => updateChange(e.target)}></IonInput>
+                  </IonItem>
+
+                  <IonItem>
+                    <IonButton onClick={e => updateExpense(photo.filepath, expense)}>Update</IonButton>
+                  </IonItem>
+
+                </IonCol>
+              </IonCardContent>
+
+            </IonCard>
+          ))}
+
+
+        </IonRow>
+      </IonGrid>
 
       <IonContent>
-        <IonTitle>
-          Expense Total: £{photos.reduce(
-          (accumulator, currentValue) => parseFloat(accumulator.toString()) + parseFloat(currentValue.expense)
-          , 0
-        )}
-        </IonTitle>
-
         <IonFab vertical="bottom" horizontal="center" slot="fixed">
           <IonFabButton onClick={() => takePhoto()}>
             <IonIcon icon={camera}></IonIcon>
